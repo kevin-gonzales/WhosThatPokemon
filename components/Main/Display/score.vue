8befaac23ffa51e9fac881f8295fa81fb5aa4e4a
Kévin Gonzales <gonzales.kvn@gmail.com>
2024-02-07 16:34:48 +0100

    add MainDisplayScore

---
+++ components/Main/Display/score.vue
@@ -0,0 +1,26 @@
+<template>
+  <span class="text-4xl text-red-500"> {{ score }} </span>
+</template>
+
+<script>
+import { useScoreStore } from "@/store/score.js";
+
+export default {
+  name: "MainDisplayScore",
+  setup() {
+    const store = useScoreStore()
+    const score = ref(store.score)
+
+    watch(() => store.score, (newValue) => {
+    score.value = newValue
+  })
+
+    return {
+        score
+    }
+  },
+};
+</script>
+
+<style>
+</style>
